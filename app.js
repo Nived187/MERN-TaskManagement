f05f8d83c78cade81ebde34f9fea145e8a0f4e48
Nived187 <nived187@outlook.com>
2023-05-18 15:54:30 +0530

    succesfully added front end with working create and get

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const dbconnect = require('./connect')
 const express = require('express')
 const tasks = require('./routes')
+const webapp = require('./webapp')
 const app = express()
 const PORT = 3000
 
@@ -10,6 +11,25 @@ const PORT = 3000
 
 app.use(express.static('./public'))
 app.use(express.json())
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 //
 
@@ -28,8 +48,7 @@ try{
 start()
 
 app.use('/api/tasks',tasks)
-app.get('/',(req,res)=>{
-    res.sendFile('./public/home.html')
-})
+app.use('/home',webapp)
+
 
 // */
